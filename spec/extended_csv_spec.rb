98cd3634926ac11ad936535dde20b5412478d74b
Jeff Iacono <jeff.iacono@gmail.com>
2013-06-18 23:34:02 -0700

    Reordering and minor code refactoring

--- spec/extended_csv_spec.rb
+++ spec/extended_csv_spec.rb
@@ -12,32 +12,11 @@ def odd_number_of_records_csv_filepath
   File.expand_path(File.join("..", "support", "odd_number_of_records.csv"), __FILE__)
 end
 
+def safely_rm file
+  FileUtils.rm file if File.exists? file
+end
+
 describe ExtendedCSV do
-  describe "#==" do
-    let(:extended_csv)         { described_class.new }
-    let(:another_extended_csv) { described_class.new }
-
-    context "when the content is the same" do
-      before do
-        extended_csv.filepaths         = sample_csv_filepath
-        another_extended_csv.filepaths = sample_csv_filepath
-      end
-
-      subject { extended_csv == another_extended_csv }
-      it      { expect(subject).to be_true }
-    end
-
-    context "when the content is different" do
-      before do
-        extended_csv.filepaths         = sample_csv_filepath
-        another_extended_csv.filepaths = another_sample_csv_filepath
-      end
-
-      subject { extended_csv == another_extended_csv }
-      it      { expect(subject).to be_false }
-    end
-  end
-
   describe "#+" do
     let(:extended_csv)         { described_class.new sample_csv_filepath }
     let(:another_extended_csv) { described_class.new another_sample_csv_filepath }
@@ -68,6 +47,31 @@ describe ExtendedCSV do
                                     ["row_2_column_1_entry", "row_2_column_2_entry"],
                                     ["row_1_column_1_entry_diff_content", "row_1_column_2_entry_diff_content"],
                                     ["row_2_column_1_entry_diff_content", "row_2_column_2_entry_diff_content"]]
+    end
+  end
+
+  describe "#==" do
+    let(:extended_csv)         { described_class.new }
+    let(:another_extended_csv) { described_class.new }
+
+    context "when the content is the same" do
+      before do
+        extended_csv.filepaths         = sample_csv_filepath
+        another_extended_csv.filepaths = sample_csv_filepath
+      end
+
+      subject { extended_csv == another_extended_csv }
+      it      { expect(subject).to be_true }
+    end
+
+    context "when the content is different" do
+      before do
+        extended_csv.filepaths         = sample_csv_filepath
+        another_extended_csv.filepaths = another_sample_csv_filepath
+      end
+
+      subject { extended_csv == another_extended_csv }
+      it      { expect(subject).to be_false }
     end
   end
 
@@ -102,23 +106,6 @@ describe ExtendedCSV do
 
     it "equals the number of entries" do
       expect(subject).to eq 3
-    end
-  end
-
-  describe "#write!" do
-    let(:infile)       { sample_csv_filepath }
-    let(:outfile)      { File.expand_path(File.join("..", "support", "test", "write_sample.csv"), __FILE__) }
-    let(:extended_csv) { described_class.new infile }
-
-    before  { FileUtils.rm outfile if File.exists? outfile }
-    after   { FileUtils.rm outfile }
-
-    subject { extended_csv.write! outfile }
-
-    it "writes the file to the specified location" do
-      expect(File.exists? outfile).to be_false
-      subject
-      expect(FileUtils.identical? infile, outfile).to be_true
     end
   end
 
@@ -199,6 +186,23 @@ describe ExtendedCSV do
                                          ["odd_row_2_column_1_entry", "odd_row_2_column_2_entry"],
                                          ["odd_row_3_column_1_entry", "odd_row_3_column_2_entry"]]
       end
+    end
+  end
+
+  describe "#write!" do
+    let(:infile)       { sample_csv_filepath }
+    let(:outfile)      { File.expand_path(File.join("..", "support", "test", "write_sample.csv"), __FILE__) }
+    let(:extended_csv) { described_class.new infile }
+
+    before  { safely_rm outfile }
+    after   { safely_rm outfile }
+
+    subject { extended_csv.write! outfile }
+
+    it "writes the file to the specified location" do
+      expect(File.exists? outfile).to be_false
+      subject
+      expect(FileUtils.identical? infile, outfile).to be_true
     end
   end
 end
